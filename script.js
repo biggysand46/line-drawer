5300184f11f727a056d1a8e1d303e5e5cdf2fd5d
biggysand46 <130106790+biggysand46@users.noreply.github.com>
2024-07-29 23:41:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ function random_number(max) {
 }
 
 for (let i = 0; i < circle_numbers; i++) {
-    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 
     random_x = random_number(1880)+20;
     random_y = random_number(1000)+20;
 
-    circle.setAttribute('r', '2');
-    circle.setAttribute('cx', random_x.toString());
-    circle.setAttribute('cy', random_y.toString());
-    circle.setAttribute('stroke-width', '3');
-    circle.setAttribute('fill', 'white');
-    circle.setAttribute('class', 'circle');
+    circle.setAttribute("r", "2");
+    circle.setAttribute("cx", random_x.toString());
+    circle.setAttribute("cy", random_y.toString());
+    circle.setAttribute("stroke-width", "3");
+    circle.setAttribute("fill", "white");
+    circle.setAttribute("class", "circle");
 
     container.appendChild(circle);
 }
@@ -38,7 +38,7 @@ for (let i = 0; i < circle_numbers; i++) {
         ccx = parseInt(container.children[v].getAttribute("cx"));
         ccy = parseInt(container.children[v].getAttribute("cy"));
 
-        distance = Math.sqrt(((ccx-cx)**2) - ((ccy-cy)**2));
+        distance = Math.sqrt(((ccx-cx)**2) + ((ccy-cy)**2));
 
         if (distance < closest) {
             closest = parseInt(distance);
@@ -46,12 +46,12 @@ for (let i = 0; i < circle_numbers; i++) {
             closesty = ccy;
         }
     }
-    var line = document.createElementNS('http://www.w3.org/2000/svg','line');
+    var line = document.createElementNS("http://www.w3.org/2000/svg","line");
 
-    line.setAttribute('x1', cx);
-    line.setAttribute('y1', cy);
-    line.setAttribute('x2', closestx);
-    line.setAttribute('y2', closesty);
+    line.setAttribute("x1", cx);
+    line.setAttribute("y1", cy);
+    line.setAttribute("x2", closestx);
+    line.setAttribute("y2", closesty);
     line.setAttribute("stroke", "white");
 
     container.append(line);
